a8f4d2217d3ac6114346c638ab4db4751aac29f5
khanzele16 <khanzele16@gmail.com>
2025-08-11 00:26:07 +0300

    First Commit

--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-import { start, help } from "../lib/commands";
-import { ICommand } from "../types";
-
-export const commands: ICommand[] = [
-  { command: "start", description: "Запустить бота", action: start },
-  { command: "help", description: "Помощь", action: help },
-];
